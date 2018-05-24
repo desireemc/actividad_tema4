00f6ec8a76553bb3bc2502ad7065fb75c6b6099f
Desiree <desire-mc@hotmail.com>
2018-05-24 20:23:51 +0200

    Cambios Modificando error

--- Clase1.java
+++ Clase1.java
@@ -1,13 +1,35 @@
-int variable1;
-int variable2;
+public class Clase1{
 
-public int getVariable1
-	return variable1;
-}
+private int variable1;
+private int variable2;
+private int variable3;
+
+
+public Clase1(int variable1, int variable2, int variable3){
+	this.variable1=variable1;
+	this.variable2=variable2;
+	this.variable3=variable3;
 
 public void setVariable1(int variable1){
 
 	this.variable1=variable1;
+}
+public int  getVariable1(int variable1){
+	return variable1;
+}
+public void setVariable2(int variable2){
+	this.variable2=variable2;
+}
 
+public int getVariable2(int variable2){
+	return variable2;
+}
 
+public void setVariable3(int variable1){
+	this.variable1=variable1;
+}
+
+public int getVariable3(int variable3){
+	return variable3;
+}
 
--- Clase2.java
+++ Clase2.java
@@ -1,2 +1,10 @@
 int variable3;
 
+public int getVariable3(){
+	return variable3;
+
+public void setVariable3(int variable3){
+	this.variable1=variable1;
+}
+
+
